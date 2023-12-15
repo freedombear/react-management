e54121e3c29ef3c0aa1fa0e04641ce7ccfc606ff
freedombear1 <757418881@qq.com>
2023-12-15 19:10:57 +0800

    lesson 5

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react' // 导入React模块
+import ReactDOM from 'react-dom/client' // 导入ReactDOM模块
+// 样式初始化一般放在最前
+import "reset-css"
+// UI框架样式放这里
 
+// 组件的样式放这里
+import App from './App.tsx' // 导入App组件
+
+// 渲染根组件
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // 严格模式
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 )
